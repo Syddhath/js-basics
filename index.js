811627e5c89f59cd59970209b3b29d0f25f18edf
Siddharth Parmar <siddharthparmarone@gmail.com>
2023-06-20 15:34:56 +1000

    Digital Clock

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function greet(name) {
-  console.log("hello world" + name);
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h -= 12;
+    ampm = "PM";
+  }
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
 }
 
-function square(number) {
-  return number * number;
-}
-
-console.log(square(4));
+updateClock();
